3e9b5621196706e3e7a6fbf4c28b67b505f4138a
Jamie Rolfs <jamie.rolfs@gmail.com>
2019-08-06 17:10:22 -0700

    feat(lint): configure linting using Airbnb + TypeScript + Prettier configs/plugins

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -1,11 +1,20 @@
 const {ifAnyDep} = require('../utils')
 
+const withBaseConfig = base => variant =>
+  require.resolve(base + (variant ? `/${variant}` : ''))
+
+const airbnb = withBaseConfig('eslint-config-airbnb-typescript')
+const prettier = withBaseConfig('eslint-config-prettier')
+
 module.exports = {
+  plugins: require.resolve('eslint-plugin-prettier'),
   extends: [
-    require.resolve('eslint-config-kentcdodds'),
-    require.resolve('eslint-config-kentcdodds/jest'),
-    ifAnyDep('react', require.resolve('eslint-config-kentcdodds/jsx-a11y')),
-    ifAnyDep('react', require.resolve('eslint-config-kentcdodds/react')),
+    ifAnyDep('react', airbnb(), airbnb('base')),
+    prettier(),
+    prettier('@typescript-eslint'),
+    ifAnyDep('react', prettier('react')),
   ].filter(Boolean),
-  rules: {},
+  rules: {
+    'prettier/prettier': 'error',
+  },
 }
