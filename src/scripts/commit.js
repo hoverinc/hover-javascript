462c5e7b1978af9bd20eb47ae6bb2ea33b21b8ff
Jamie Rolfs <jamie.rolfs@gmail.com>
2019-09-16 16:19:36 -0700

    fix(scripts/commit): find commitizen relative to parent project as well

--- src/scripts/commit.js
+++ src/scripts/commit.js
@@ -1,10 +1,17 @@
+const fs = require('fs')
 const path = require('path')
 const {bootstrap} = require('commitizen/dist/cli/git-cz')
+const {fromRoot} = require('../utils')
 
 const here = p => path.join(__dirname, p)
 
+const cliPath = [
+  here('../../node_modules/commitizen'),
+  fromRoot('./node_modules/commitizen'),
+].find(fs.existsSync)
+
 bootstrap({
-  cliPath: here('../../node_modules/commitizen'),
+  cliPath,
   config: {
     path: '@commitlint/prompt',
   },
