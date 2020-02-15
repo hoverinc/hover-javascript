2df3f352cb32240c820546bfcbe5dc38950290b4
Jamie Rolfs <jamie.rolfs@gmail.com>
2020-02-15 01:39:27 -0800

    build: add scripts + configs as commitlint scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,20 @@
+const {readdirSync, statSync} = require('fs')
+const {join, sep} = require('path')
+
+const ls = path =>
+  readdirSync(path)
+    .filter(f => statSync(join(path, f)).isFile())
+    .map(
+      f => `${path.split(sep).reverse()[0]}/${f.replace(/(.config)?.js/, '')}`,
+    )
+
 module.exports = {
   extends: ['./src/config/commitlint.config'],
+  rules: {
+    'scope-enum': [
+      1,
+      'always',
+      [...ls('./src/config'), ...ls('./src/scripts'), 'deps', 'build'],
+    ],
+  },
 }
