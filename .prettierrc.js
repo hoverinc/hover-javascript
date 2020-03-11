e90cf357140ff01ad9f5ab91de4ca87623f781b3
Jamie Rolfs <jamie.rolfs@gmail.com>
2020-03-10 19:35:22 -0700

    fix: use upstream instead of shared Prettier configuration for this repo
    Use Prettier configuration from upstream repository (https://github.com/kentcdodds/kcd-scripts) instead of the shared configuration distributed with this package from `src/config/prettierrc.js` in order to maintain the ability to merge the upstream repo.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,1 +1,18 @@
-module.exports = require('./src/config/prettierrc');
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: false,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
