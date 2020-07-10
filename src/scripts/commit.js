084fe88beda22d90ea03ad433d7798924a434d4c
Jamie Rolfs <jamie.rolfs@gmail.com>
2020-07-10 13:13:58 -0700

    fix(scripts/commit): strip rogue commit arg coming from yarn commit
    For some reason invoking this script with `yarn commit` now passes through `'commit'` in the arguments that get parsed as "raw git arguments" in the adapter which blows up said parsing behavior. I'm not sure if this was related to a yarn change (`yarn [script] args` vs. `yarn [script] -- args`) or something else...
    
    Either way, this is a band-aid to filter out the rogue argument for now.

--- src/scripts/commit.js
+++ src/scripts/commit.js
@@ -10,9 +10,22 @@ const cliPath = [
   fromRoot('./node_modules/commitizen'),
 ].find(fs.existsSync)
 
-bootstrap({
-  cliPath,
-  config: {
-    path: '@commitlint/prompt',
+// eslint-disable-next-line no-warning-comments
+// FIXME: for some reason invoking this script with `yarn commit` now passes
+// through `'commit'` in the arguments that get parsed as "raw git arguments"
+// in the adapter which blows up said parsing behavior. I'm not sure if this
+// was related to a yarn change (`yarn [script] args` vs. `yarn [script] --
+// args`) or something else...
+//
+// Either way, this is a band-aid to filter out the rogue argument for now.
+const args = process.argv.filter(arg => arg !== 'commit')
+
+bootstrap(
+  {
+    cliPath,
+    config: {
+      path: '@commitlint/prompt',
+    },
   },
-})
+  args,
+)
