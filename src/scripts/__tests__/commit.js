7a142092af404babd7a95b2f2c8f64f39b5f91ff
Jamie Rolfs <jamie.rolfs@gmail.com>
2020-07-10 14:18:45 -0700

    test(scripts/commit): add tests for git-cz `bootstrap` invocation

---
+++ src/scripts/__tests__/commit.js
@@ -0,0 +1,62 @@
+import cases from 'jest-in-case'
+import {
+  unquoteSerializer,
+  winPathSerializer,
+  relativePathSerializer,
+} from './helpers/serializers'
+
+jest.mock('commitizen/dist/cli/git-cz')
+
+expect.addSnapshotSerializer(unquoteSerializer)
+expect.addSnapshotSerializer(winPathSerializer)
+expect.addSnapshotSerializer(relativePathSerializer)
+
+cases(
+  'commit',
+  ({args = [], env = {}}) => {
+    // beforeEach
+    const {bootstrap: bootstrapMock} = require('commitizen/dist/cli/git-cz')
+
+    const originalArgv = process.argv
+    const originalExit = process.exit
+    const originalEnv = process.env
+
+    process.exit = jest.fn()
+    process.argv = ['node', '../commit', ...args]
+    process.env = env
+
+    try {
+      // tests
+      require('../commit')
+
+      expect(bootstrapMock).toHaveBeenCalledTimes(1)
+
+      const [call] = bootstrapMock.mock.calls
+      const [optionsArg, argsArg] = call
+
+      expect(optionsArg).toMatchSnapshot()
+      expect(argsArg.join(' ')).toMatchSnapshot()
+    } catch (error) {
+      throw error
+    } finally {
+      // afterEach
+      process.exit = originalExit
+      process.argv = originalArgv
+      process.env = originalEnv
+
+      jest.resetModules()
+    }
+  },
+  {
+    'bootstraps @commitlint/prompt': {},
+    'strips errant "commit" argument': {
+      args: ['commit'],
+    },
+    'forwards arguments': {
+      args: ['--retry'],
+    },
+    'strips errant "commit" argument and forwards arguments': {
+      args: ['commit', '--retry'],
+    },
+  },
+)
