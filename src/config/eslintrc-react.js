c3703624f927b04d348231a3d1f0823a412f13bf
Jamie Rolfs <jamie.rolfs@gmail.com>
2020-07-14 20:52:25 -0700

    Revert "feat: expose 'builders' and `tsProjects` option from ESLint config"
    This reverts commit 77bb2d0f6ddd7e955c2d6b6d226168dd8ab762f3.

--- src/config/eslintrc-react.js
+++ src/config/eslintrc-react.js
@@ -1,10 +1,3 @@
 const {buildConfig} = require('./helpers/eslint')
 
-const defaultOptions = {withReact: true}
-
-const defaultExport = buildConfig(defaultOptions)
-
-defaultExport.buildConfig = options =>
-  buildConfig({...defaultOptions, ...options})
-
-module.exports = defaultExport
+module.exports = buildConfig({withReact: true})
--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -1,7 +1,3 @@
 const {buildConfig} = require('./helpers/eslint')
 
-const defaultExport = buildConfig()
-
-defaultExport.buildConfig = buildConfig
-
-module.exports = defaultExport
+module.exports = buildConfig()
