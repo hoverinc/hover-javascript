ccb4e76d5ef696e1dc9031e31a373d425ad1c015
Max Summe <msumme@gmail.com>
2020-08-31 16:04:45 -0700

    Update src/config/lintstagedrc.js
    Co-authored-by: Jamie Rolfs <jrolfs@users.noreply.github.com>

--- src/config/lintstagedrc.js
+++ src/config/lintstagedrc.js
@@ -5,12 +5,10 @@ const doctoc = resolveBin('doctoc')
 
 module.exports = {
   'README.md': [`${doctoc} --maxlevel 3 --notitle`],
-  '*.+(json|yml|yaml|css|less|scss|md|graphql|mdx|vue)': [
+  '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx|vue)': [
     `${hoverScripts} format`,
-    `${hoverScripts} test --findRelatedTests`,
   ],
   '*.+(js|jsx|ts|tsx)': [
-    `${hoverScripts} format`,
     `${hoverScripts} lint`,
     `${hoverScripts} test --findRelatedTests`,
   ],
