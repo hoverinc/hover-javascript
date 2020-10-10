67cdf48f53b0ce348686991c6368392060a2c75c
Jamie Rolfs <jamie.rolfs@gmail.com>
2020-10-10 16:44:40 -0700

    feat: 🔪 remove build script and related configuration
    We don't use any of the build-related tooling that this library exports. This
    removes the script and all related configuration and dependencies and replaces
    the build process with a simple Babel CLI based build.
    
    BREAKING CHANGE: This removes the `hover-scripts build` script, the exported
    Babel configuration, and the exported Rollup configuration. It also removes the
    built-in Babel integration in the Jest configuation.

--- src/config/__tests__/umbrella.js
+++ src/config/__tests__/umbrella.js
@@ -1,11 +1,7 @@
 test('requiring some files does not blow up', () => {
-  require('../babel-transform')
-  require('../babelrc')
   require('../eslintrc')
   require('../eslintrc-react')
   require('../jest.config')
   require('../lintstagedrc')
   require('../prettierrc')
-  require('../rollup.config')
-  require('../').getRollupConfig()
 })
