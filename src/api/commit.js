dd996e8ea6a02d8d903f279a04d777cb4f842cee
Jamie Rolfs <jamie.rolfs@gmail.com>
2020-10-11 00:28:42 -0700

    feat(api): add `commit` API module with commitlint scope helper

---
+++ src/api/commit.js
@@ -0,0 +1,31 @@
+const {readdirSync, statSync} = require('fs')
+const {join, sep} = require('path')
+
+/**
+ * Enumerate config files
+ *
+ * Example
+ *
+ * - `dir/foo.js`
+ * - `dir/bar.config.js`
+ * - `dir/baz.json`
+ * - `dir/qux.ts`
+ *
+ * `ls.config('./dir') → ['dir/foo', 'dir/bar', 'dir/baz', 'dir/qux']`
+ *
+ * @param {string} path - directory to enumerate
+ */
+const configs = path =>
+  readdirSync(path)
+    .filter(f => statSync(join(path, f)).isFile())
+    .map(
+      f =>
+        `${path.split(sep).reverse()[0]}/${f.replace(
+          /(\.config)?.(json|js|ts)/,
+          '',
+        )}`,
+    )
+
+const ls = {configs}
+
+module.exports = {ls}
