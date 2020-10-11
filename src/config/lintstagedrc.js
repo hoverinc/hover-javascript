bf1b44b2413ad83ca4d454e89c097d1b230a2ac8
Jamie Rolfs <jamie.rolfs@gmail.com>
2020-10-11 01:36:20 -0700

    feat(config/lintstagedrc): update max DocToc depth to 4

--- src/config/lintstagedrc.js
+++ src/config/lintstagedrc.js
@@ -4,7 +4,7 @@ const hoverScripts = resolveHoverScripts()
 const doctoc = resolveBin('doctoc')
 
 module.exports = {
-  'README.md': [`${doctoc} --maxlevel 3 --notitle`],
+  'README.md': [`${doctoc} --maxlevel 4 --notitle`],
   '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx|vue)': [
     `${hoverScripts} format`,
   ],
