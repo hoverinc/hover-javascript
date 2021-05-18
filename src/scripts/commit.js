cba99977971503fcb4a49b2d2fcfe311e0dfec03
Jamie Rolfs <jamie.rolfs@gmail.com>
2021-05-17 22:30:24 -0700

    fix: pass correct arguments to sub-script
    Update argument forwarding logic in run-script wrapper to dynamically
    find sub-script command and therefore pass correct arguments to script
    withou hard-coding the script names. I was not aware of this hard-coded
    bit and therefore some scripts were receiving the sub-script command
    erroneously.

--- src/scripts/commit.js
+++ src/scripts/commit.js
@@ -3,16 +3,6 @@ const {bootstrap} = require('commitizen/dist/cli/git-cz')
 const commitizenPaths = require.resolve('commitizen/package.json').split('/')
 const cliPath = commitizenPaths.slice(0, -1).join('/')
 
-// eslint-disable-next-line no-warning-comments
-// FIXME: for some reason invoking this script with `yarn commit` now passes
-// through `'commit'` in the arguments that get parsed as "raw git arguments"
-// in the adapter which blows up said parsing behavior. I'm not sure if this
-// was related to a yarn change (`yarn [script] args` vs. `yarn [script] --
-// args`) or something else...
-//
-// Either way, this is a band-aid to filter out the rogue argument for now.
-const args = process.argv.filter(arg => arg !== 'commit')
-
 bootstrap(
   {
     cliPath,
@@ -20,5 +10,5 @@ bootstrap(
       path: '@commitlint/prompt',
     },
   },
-  args,
+  process.argv,
 )
