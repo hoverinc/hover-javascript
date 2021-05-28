5a33d35f65a6b1f70bc8998a0f5b537c479f4923
Jamie Rolfs <jamie.rolfs@gmail.com>
2021-05-28 12:46:10 -0700

    build: drop support for Node 10
    BREAKING CHANGE: drop support for Node 10

--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@
 let shouldThrow = false
 
 try {
-  const [major, minor] = process.version
-    .slice(1)
-    .split('.')
-    .map(Number)
+  const [major] = process.version.slice(1).split('.').map(Number)
   shouldThrow =
     require(`${process.cwd()}/package.json`).name === '@hover/javascript' &&
-    (major < 10 || (major === 10 && minor < 18))
+    major < 12
 } catch (error) {
   // ignore
 }
 
 if (shouldThrow) {
   throw new Error(
-    'You must use Node version 10.18 or greater to run the scripts within @hover/javascript, because we dogfood the untranspiled version of the scripts.',
+    'You must use Node version 12 or greater to run the scripts within @hover/javascript, because we dogfood the untranspiled version of the scripts.',
   )
 }
 
