bf86aac91755d6fc06de13283d0879bc9723b1cf
Jamie Rolfs <jamie.rolfs@hover.to>
2022-09-07 00:44:37 -0700

    feat(config/test): use **@swc/jest** as default Jest transform
    BREAKING CHANGE: **@swc/jest** is now the default Jest transform and
    **ts-jest** is no longer installed by default. To preserve the old transform
    behavior, install **ts-jest** as a \`devDependency\` in you project. ∙ BREAKING
    CHANGE: the \`@hover/javascript/api/test\` API has been removed as **ts-jest**
    is no longer installed by default. If you're still using **ts-jest**, import
    its helpers directly from **ts-jest** instead of the \`/api/test\` entrypoint.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 const {
   globals,
-  transform,
   transformIgnorePatterns,
   ...config
 } = require('./src/config/jest.config')
@@ -11,13 +10,4 @@ module.exports = {
   roots: ['<rootDir>/src'],
   coverageThreshold: null,
   transformIgnorePatterns: [...transformIgnorePatterns, '.prettierrc.js'],
-  // Specifying ts-jest options via `global` in Jest configuration has been
-  // deprecated so we have to do this in order to add the `exclude` option to
-  // the transform in our Jest configuration that already has `transform` ☹
-  transform: Object.fromEntries(
-    Object.entries(transform).map(([glob, [transformer, options]]) => [
-      glob,
-      [transformer, {...options, exclude: '**/*'}],
-    ]),
-  ),
 }
--- src/utils.js
+++ src/utils.js
@@ -244,6 +244,7 @@ module.exports = {
   getConcurrentlyArgs,
   getPkgName,
   hasAnyDep,
+  hasDevDep,
   hasFile,
   hasLocalConfig,
   hasPkgProp,
