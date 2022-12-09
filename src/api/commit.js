8648a39bc8f838d6ef5ffd2ac9a95294440107af
Jamie Rolfs <jamie.rolfs@gmail.com>
2022-12-09 13:00:27 -0800

    feat(api/commit): add support for excluding directories in `ls.dirs()`
    BREAKING CHANGE: `ls.dirs()` from `api/commit` now takes an options
    object as the second parameter instead of a `prefix`. If you're using
    a prefix you'll need to make the following change:
    
    ```ts
    // Before
    ls.dirs('./packages', 'my-prefix');
    
    // After
    ls.dirs('./packages', { prefix: 'my-prefix' });
    ```
    
    BREAKING CHANGE: `ls.dirs()` now excludes `node_modules` directories
    by default. If for some reason you want to include \`node_modules\`,
    you'll need to explicitly set \`exclude\` to \`null\`:
    
    ```ts
    // Before
    ls.dirs('./packages');
    
    // After
    ls.dirs('./packages', { exclude: null });
    ```

--- src/api/commit.js
+++ src/api/commit.js
@@ -45,16 +45,38 @@ const configs = path =>
     )
 
 /**
+ * @typedef DirsOptions
+ * @property {string} [prefix] prefix to prepend to each scope entry (e.g:
+ * `{ prefix: 'prefix' }` becomes `type(prefix/scope)`)
+ * @property {RegExp | string | null} [exclude] expression for excluding
+ * directories, defaults to `^node_modules`
+ */
+
+/**
  * Enumerate one level of directories
  *
  * @param {string} path - directory to enumerate
- * @param {string} [prefix] - prefix for each scope entry (e.g: `prefix/entry`)
+ * @param {DirsOptions} options -
  */
-const dirs = (path, prefix) =>
-  readdirSync(path)
-    .filter(f => statSync(join(path, f)).isDirectory())
+const dirs = (path, options) => {
+  const {exclude, prefix} = {exclude: /^node_modules/, ...options}
+
+  /**
+   * @param {string} f filename
+   */
+  const test = f => {
+    if (!exclude) return true
+
+    return !(typeof exclude === 'string'
+      ? f.includes(exclude)
+      : exclude.test(f))
+  }
+
+  return readdirSync(path)
+    .filter(f => statSync(join(path, f)).isDirectory() && test(f))
     .map(item => (prefix ? `${prefix}/${item}` : item))
     .map(item => item.toLowerCase())
+}
 
 const ls = {configs, dirs}
 
